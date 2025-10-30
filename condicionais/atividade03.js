84876078c5aff64dc8b87978b2657835465f9520
julirmatos <juli.rmatos@gmail.com>
2025-10-30 15:44:03 -0300

    atividade 3

--- condicionais/atividade03.js
+++ condicionais/atividade03.js
@@ -2,21 +2,18 @@ const leia = require('readline-sync')
 
 let nome = leia.question('Digite o Nome do doador: ')
 let idade = leia.questionInt('Digite a Idade do doador: ')
-let primeira = leia.question('Primeira doação de sangue? (true/false): ')
-
-let primeiraDoacao = (primeira.toLowerCase() === 'true')
+let doacao = leia.question('Primeira doação de sangue? (S/N): ').toUpperCase()
 
 let apto = false
 
 if (idade >= 18 && idade <= 69) {
-    if (idade >= 60 && primeiraDoacao) {
-        apto = false 
+    if (idade >= 60 && doacao === 'S') {
+        apto = false  
     } else {
-        apto = true 
+        apto = true   
     }
 }
 
-// Saída
 if (apto) {
     console.log("\n" + nome + " está apto para doar sangue!")
 } else {
