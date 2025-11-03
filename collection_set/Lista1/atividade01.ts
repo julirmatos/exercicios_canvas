f7c2877c9fce34efd30b55616cc8f910c1a5d7bf
julirmatos <juli.rmatos@gmail.com>
2025-11-03 16:11:46 -0300

    Collection Set

---
+++ collection_set/Lista1/atividade01.ts
@@ -0,0 +1,17 @@
+const leia = require("readline-sync")
+
+const cores: Array<string> = new Array<string>()
+
+for (let contadori: number = 0; contador < 6; i++) {
+    let cor: string = leia.question(`Digite a ${contador}º cor: `)
+    cores.push(cor)
+}
+console.log(`As cores digitadas foram: ${cores}`)
+
+for (let cor of cores)
+    console.log(cor)
+
+cores.sort()
+console.log(`As cores em ordem alfabética são: ${cores}`)
+for (let cor of cores)
+    console.log(cor)
